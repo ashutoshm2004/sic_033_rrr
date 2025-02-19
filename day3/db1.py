f5c6595627eb02f8af8999e1495d9b998befed21
ashutoshm2004 <ashutm59@gmail.com>
2025-02-19 16:38:14 +0530

    FUCK

--- day3/db1.py
+++ day3/db1.py
@@ -2,19 +2,36 @@ import pymysql
 
 def connect_db():
     try:
-        connection = pymysql.Connect(host='localhost', port=3306, user='root', password='Root12355', database='nithin_db', charset='utf8')
-        print('DB connected')
+        connection = pymysql.Connect(host='localhost', port=34306, user='root', password='abcd1234', database='persons')
+        print('Database Connected')
         return connection
     except:
-        print('DB connection failed')
+        print("Connection failed")  
 
 def disconnect_db(connection):
-    try:
-        connection.close()
-        print('DB dis-connected')
-    except:
-        print('Error while disconnecting DB')
+    connection.close()
+    print('Database Disconnected')
 
-connection = connect_db()
-if connection:
+def create_db():
+    connection = connect_db()
+    query = 'create database IF NOT EXISTS students;'
+    cursor = connection.cursor()
+    cursor.execute(query)
+    cursor.close()
     disconnect_db(connection)
+
+def create_db():
+    connection = connect_db()
+    query = ''''create table IF NOT EXISTS stud(id INT, name VARCHAR(50), sem INT, gpa DECIMAL(3,2), gender CHAR check(gender IN ('M','F')));'''
+    cursor = connection.cursor()
+    cursor.execute(query)
+    cursor.close()
+    disconnect_db(connection)
+
+# def insert_row():
+    
+
+create_db()
+insert_row()
+# if connection:
+#     disconnect_db(connection)
