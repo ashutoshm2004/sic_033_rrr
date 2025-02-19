b9d257a9f2f898b6bb8d6e634f1e238fb9e8d422
ashutoshm2004 <ashutm59@gmail.com>
2025-02-19 15:52:42 +0530

    minor change

--- day3/db1.py
+++ day3/db1.py
@@ -3,9 +3,9 @@ def connect_db():
     try:
         connection = pymysql.Connect(host='localhost', port=3306, user='root', password='abcd1234', database='persons')
         print('Database Connected')
+        return connection
     except:
-        print("Connection failed")
-    return connection
+        print("Connection failed")  
 
 def disconnect_db(connection):
     connection.close()
