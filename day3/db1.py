d867fc2e0d08de5b9a746d350157d43147c0d34d
ashutoshm2004 <ashutm59@gmail.com>
2025-02-20 09:08:43 +0530

    db1 added

--- day3/db1.py
+++ day3/db1.py
@@ -2,36 +2,19 @@ import pymysql
 
 def connect_db():
     try:
-        connection = pymysql.Connect(host='localhost', port=34306, user='root', password='abcd1234', database='persons')
-        print('Database Connected')
+        connection = pymysql.Connect(host='localhost', port=3306, user='root', password='abcd1234', database='persons', charset='utf8')
+        print('DB connected')
         return connection
     except:
-        print("Connection failed")  
+        print('DB connection failed')
 
 def disconnect_db(connection):
-    connection.close()
-    print('Database Disconnected')
+    try:
+        connection.close()
+        print('DB dis-connected')
+    except:
+        print('Error while disconnecting DB')
 
-def create_db():
-    connection = connect_db()
-    query = 'create database IF NOT EXISTS students;'
-    cursor = connection.cursor()
-    cursor.execute(query)
-    cursor.close()
+connection = connect_db()
+if connection:
     disconnect_db(connection)
-
-def create_db():
-    connection = connect_db()
-    query = ''''create table IF NOT EXISTS stud(id INT, name VARCHAR(50), sem INT, gpa DECIMAL(3,2), gender CHAR check(gender IN ('M','F')));'''
-    cursor = connection.cursor()
-    cursor.execute(query)
-    cursor.close()
-    disconnect_db(connection)
-
-# def insert_row():
-    
-
-create_db()
-insert_row()
-# if connection:
-#     disconnect_db(connection)
