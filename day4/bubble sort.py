742f55fad5fe66edda5afac27a845218e7ec298b
ashutoshm2004 <ashutm59@gmail.com>
2025-02-20 12:25:45 +0530

    case

--- day4/bubble sort.py
+++
@@ -1,9 +0,0 @@
-a=[9,7,7,3,6,2,4,2,4,8]
-n=len(a)
-for i in range(0,n-1):
-    for j in range(0,n-1-i):
-        if a[j]>a[j+1]:
-            temp=a[j]
-            a[j]=a[j+1]
-            a[j+1]=temp
-print(a)
